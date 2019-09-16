69f1fe64c604f72f3f4389e1a44f571fdfa908d9
n.akhmetov <n.akhmetov@corp.mail.ru>
2019-09-16 16:51:42 +0300

    Horizontal or vertical only sizing.

--- code.ts
+++ code.ts
@@ -12,19 +12,39 @@ function isConstrainedLayoutMixin(node: BaseNode):Boolean {
 
 let note = null;
 
+const CommandHorizontal = "h";
+const CommandVertical = "v";
+
 if (selection.length > 0) {
   let unframed:SceneNode[] = [];
   selection.forEach(node => {
     if (isConstrainedLayoutMixin(node)) {
-      const clNode = node as ConstrainedLayoutMixin;
+      const cl = node as ConstrainedLayoutMixin;
       if (isLayoutMixin(node.parent)) {
         const container = node.parent as DefaultContainerMixin;
-        clNode.x = clNode.y = 0;
-        clNode.resize(container.width, container.height);
-        const cs = clNode.constraints;
-        if (cs.horizontal == "MIN" && cs.vertical == "MIN") {
-          clNode.constraints = { horizontal: "STRETCH", vertical: "STRETCH" };
-        }
+				const cs = cl.constraints;
+        switch (figma.command) {
+					case CommandHorizontal: {
+						cl.resize(container.width, cl.height);
+						cl.x = 0;
+						break;
+					}
+					case CommandVertical: {
+						cl.resize(cl.width, container.height);
+						cl.y = 0;
+						break;
+					}
+					default: {
+						cl.resize(container.width, container.height);
+						cl.x = cl.y = 0;
+						break;
+					}
+				}
+        switch (figma.command) {
+					case CommandHorizontal: if (cs.horizontal == "MIN") cl.constraints = { horizontal: "STRETCH", vertical: cs.vertical }; break;
+					case CommandVertical: if (cs.vertical == "MIN") cl.constraints = { horizontal: cs.horizontal, vertical: "STRETCH" }; break;
+					default: if (cs.horizontal == "MIN" && cs.vertical == "MIN") cl.constraints = { horizontal: "STRETCH", vertical: "STRETCH" }; break;
+				}
       } else {
         unframed.push(node);
       }
@@ -33,7 +53,7 @@ if (selection.length > 0) {
   switch (unframed.length) {
     case 0: break;
     case 1: note = `${unframed[0].name} isn't`; break;
-    case 2: case 3: case 4: 
+    case 2: case 3: case 4:
       note = `${unframed.slice(0, unframed.length - 1).map((node) => node.name).join(', ')} and ${unframed[unframed.length - 1].name} aren't`;
       break;
     default: note = `${unframed.length} elements aren't`; break;
